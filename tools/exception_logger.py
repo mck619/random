abc8e200381e4ca8f5bf000e294a143345c40619
mck619 <mckakehashi@gmail.com>
2018-03-26 15:43:30 -0700

    Update exception_logger.py

--- tools/exception_logger.py
+++ tools/exception_logger.py
@@ -8,44 +8,46 @@ class WrappedExceptionTracebackFormatter(logging.Formatter):
         tb = result_list[-1]
         tb = re.sub(r'(File.*?in wrapper\s*\n)', '',tb, flags = re.MULTILINE)
         tb = re.sub(r'(^.*?return func\(\*args, \*\*kwargs\)\s*\n)', '',tb, flags = re.MULTILINE)
-        results = '|'.join(result_list[:-1]) + '|' + tb
+        results = '|'.join(result_list[:-1]) + '| traceback: ' + tb
         return results
         
 class MySQLHandler(logging.Handler):
-    def __init__(self, sql_conn, sql_curstor, db_tbl_log):
-        logging.Handler.__init__(self)
-        self.sql_cursor = sql_cursor
-        self.sql_conn = sql_conn
-        self.db_tbl_log = db_tbl_log
+    def __init__(self, db, table, col_mapping, *args, **kwargs):
+        super(MySQLHandler, self).__init__(*args, **kwargs)
+        self.conn = pymysql.connect(user=db['user'],
+                                     password=db['password'],
+                                     host=db['host'],
+                                     database=db['database'])
+        self.cur = self.conn.cursor()        
+        self.col_mapping = col_mapping
+        self.table = table
     def emit(self, record):
-        tm = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created))
-        # Clear the log message so it can be put to db via sql (escape quotes)
-        self.log_msg = record.msg
-        self.log_msg = self.log_msg.strip()
-        self.log_msg = self.log_msg.replace('\'', '\'\'')
-        # todo :Make the SQL insert
-        """
-        example:
-        sql = 'INSERT INTO ' + self.db_tbl_log + ' (log_level, ' + \
-            'log_levelname, log, created_at, created_by) ' + \
-            'VALUES (' + \
-            ''   + str(record.levelno) + ', ' + \
-            '\'' + str(record.levelname) + '\', ' + \
-            '\'' + str(self.log_msg) + '\', ' + \
-            '(convert(datetime2(7), \'' + tm + '\')), ' + \
-            '\'' + str(record.name) + '\')'
-        """
+        msg = self.format(record)
+        msgs = msg.split('|')
+        log_data = {}
+        for msg in msgs:
+            message_type = msg[:msg.find(':')].strip()
+            message_content = msg[msg.find(':')+1:].strip()
+            if message_type in self.col_mapping.keys():
+                log_data[self.col_mapping[message_type]] = message_content
+                
+        if 'time' in self.col_mapping.keys():
+            tm = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created))
+            log_data['time']=tm
+            
+        query = """ INSERT INTO {0} """.format(self.table) + construct_insert_values_string(log_data.keys())
         try:
-            self.sql_cursor.execute(sql)
-            self.sql_conn.commit()
+            self.cur.execute(query, log_data)
+            self.conn.commit()
         # If error - print it out on screen. Since DB is not working - there's
         # no point making a log about it to the database :)
-        except pymssql.Error as e:
-            print sql
+        except pymysql.Error as e:
+            print query
+            print e
             print 'CRITICAL DB ERROR! Logging to database not possible!'
             
             
-def create_logger(name, path):
+def create_logger(name, db, table, col_mapping):
     """
     Creates a logging object and returns it
     """
@@ -53,9 +55,9 @@ def create_logger(name, path):
     logger.setLevel(logging.INFO)
  
     # create the logging file handler
-    fh = logging.FileHandler(path)
+    fh = MySQLHandler(db, table, col_mapping)
  
-    fmt = '%(asctime)s | %(name)s | %(levelname)s | %(message)s '
+    fmt ='time: %(asctime)s |logger: %(name)s |level: %(levelname)s |exception_type: %(message)s '
     formatter = WrappedExceptionTracebackFormatter(fmt)
     fh.setFormatter(formatter)
  
@@ -108,7 +110,7 @@ def add_note_to_exception():
             except Exception as e:
                 et, ei, tb = sys.exc_info()
                 err_func = func.__name__
-                raise type(e)(e.__str__() + '| ' + 'note: ' + note+ '| ' + '| function: '+err_func + '|'), tb
+                raise type(e)(e.__str__() + '| ' + 'note: ' + note + '| function: '+err_func + '|'), tb
         return wrapper
     return decorator
 
@@ -122,7 +124,7 @@ def add_kwargs_note_to_exception(*key_words):
                 err_func = func.__name__
                 note_list = [str(kwargs[key_word]) for key_word in key_words]
                 note_str = ','.join(note_list)
-                raise type(e), type(e)(e.__str__() + '| ' + 'note: ' + note_str + '| ' + '| function: '+err_func + '|'), tb
+                raise type(e), type(e)('| exception_message: ' + e.__str__() + '| ' + 'note: ' + note_str  + '| function: '+err_func + '|'), tb
         return wrapper
     return decorator    
 
