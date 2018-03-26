0339afc929146dc615a84925b781c04a81760044
mck619 <mckakehashi@gmail.com>
2018-03-26 15:42:50 -0700

    Delete mysql_tools.py

--- mysql_tools.py
+++
@@ -1,5 +0,0 @@
-def construct_insert_values_string(columns):
-    insert_str = '(' +','.join(columns) + ') '
-    value_str = ','.join(['%('+column+')s' for column in columns])
-    value_str = 'VALUES ({})'.format(value_str)
-    return insert_str + value_str
